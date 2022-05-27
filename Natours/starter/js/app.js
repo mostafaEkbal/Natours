368484a5e069b80a5f1dbe3645bd84701a516a60
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-05-27 02:03:29 +0200

    Natours project is finished el hamdolellah

--- Natours/starter/js/app.js
+++ Natours/starter/js/app.js
@@ -2,7 +2,7 @@ let viewport_location = window.visualViewport;
 const back_to_top = document.getElementById('back_to_top');
 
 window.addEventListener('scroll', function() {
-    viewport_location.pageTop === 0 ? back_to_top.classList.add('hide') : back_to_top.classList.remove('hide');
+    viewport_location.pageTop <= 500 ? back_to_top.classList.add('hide') : back_to_top.classList.remove('hide');
 });
 
 
